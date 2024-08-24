9242f089b419338f4f15585011a69d5ed46ab0fd
iamjosemorrino <newaza1599@gmail.com>
2024-08-24 13:45:41 -0400

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const con = document.getElementById("conn");
-	
-	
-	const connect = () => {
-		window.ethereum.request({method: "eth_requestAccounts"})
-		// console.log("hi");
-		
-	}
-	con.addEventListener("click", connect);
